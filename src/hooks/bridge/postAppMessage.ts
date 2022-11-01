df3c4670ec6e4f0c8f16440b5265b73a8835190a
Hyesung Oh <haesungoh414@gmail.com>
2022-11-01 10:50:11 +0900

    fix: postAppMessage value 사용하도록 수정

--- src/hooks/bridge/postAppMessage.ts
+++ src/hooks/bridge/postAppMessage.ts
@@ -1,11 +1,14 @@
+import { POST_WEBVIEW_MESSAGE_TYPE } from './constants';
 import { PostAppMessageData } from './type';
 
 /**
  * `Web`에서 `App`으로 데이터를 전송합니다.
  */
-const postAppMessage = (data: PostAppMessageData) => {
+const postAppMessage = ({ type, data, ...rest }: PostAppMessageData) => {
   if (!window.ReactNativeWebView) return;
-  window.ReactNativeWebView.postMessage(JSON.stringify(data));
+
+  const typeValue = POST_WEBVIEW_MESSAGE_TYPE[type];
+  window.ReactNativeWebView.postMessage(JSON.stringify({ type: typeValue, data, ...rest }));
 };
 
 export default postAppMessage;
