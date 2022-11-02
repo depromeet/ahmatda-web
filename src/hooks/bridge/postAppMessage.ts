c8706aa3ac3ea057320e2208ef4d7a68b2b0e899
Hyesung Oh <haesungoh414@gmail.com>
2022-11-02 21:46:02 +0900

    fix: postAppMessage undefined 핸들링

--- src/hooks/bridge/postAppMessage.ts
+++ src/hooks/bridge/postAppMessage.ts
@@ -5,7 +5,7 @@ import { PostAppMessageData } from './type';
  * `Web`에서 `App`으로 데이터를 전송합니다.
  */
 const postAppMessage = ({ type, data, ...rest }: PostAppMessageData) => {
-  if (!window.ReactNativeWebView) return;
+  if (!window?.ReactNativeWebView) return;
 
   const typeValue = POST_WEBVIEW_MESSAGE_TYPE[type];
   window.ReactNativeWebView.postMessage(JSON.stringify({ type: typeValue, data, ...rest }));
