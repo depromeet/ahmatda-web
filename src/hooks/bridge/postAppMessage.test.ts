1137340b6db6f835480c0b593a9b05ac5e01f85b
Hyesung Oh <haesungoh414@gmail.com>
2022-11-02 21:46:28 +0900

    test: postAppMessage 테스트 작성

---
+++ src/hooks/bridge/postAppMessage.test.ts
@@ -0,0 +1,42 @@
+import { POST_WEBVIEW_MESSAGE_TYPE } from './constants';
+import postAppMessage from './postAppMessage';
+
+describe('postAppMessage', () => {
+  const mockPostMessage = jest.fn();
+
+  beforeEach(() => {
+    Object.defineProperty(global, 'window', {
+      value: { ReactNativeWebView: { postMessage: mockPostMessage } },
+      writable: true,
+    });
+  });
+
+  afterEach(() => {
+    Object.defineProperty(global, 'window', { value: { ...window }, writable: true });
+  });
+
+  it('window가 undefined일 시 실행되면 안된다.', () => {
+    Object.defineProperty(global, 'window', { value: undefined, writable: true });
+    expect(window).toBeUndefined();
+
+    const result = postAppMessage({ type: 'bar', data: 'dd' });
+    expect(result).toBeUndefined();
+  });
+
+  it('window가 undefined가 아닐시 postMessage가 호출되어야 한다', () => {
+    expect(window).not.toBeUndefined();
+    // TODO: 이후 추가, 삭제되는 상수에 따라 적용
+    const obj = { type: 'bar', data: 'dd' } as const;
+
+    postAppMessage(obj);
+    expect(mockPostMessage).toBeCalled();
+  });
+
+  it('postMessage가 호출될 때 상수의 value 값으로, 문자열로 호출되어야 한다', () => {
+    const obj = { type: 'bar', data: 'dd' } as const;
+    const correctObject = { type: POST_WEBVIEW_MESSAGE_TYPE.bar, data: 'dd' };
+
+    postAppMessage(obj);
+    expect(mockPostMessage).toBeCalledWith(JSON.stringify(correctObject));
+  });
+});
