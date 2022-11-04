b60a020c96bacdbae7dfafe58ce0c40d388b6392
Hyesung Oh <haesungoh414@gmail.com>
2022-11-04 20:20:39 +0900

    test: useAppMessageListener 테스트 작성

---
+++ src/hooks/bridge/useAppMessageListener.test.ts
@@ -0,0 +1,100 @@
+import { fireEvent } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+
+import useAppMessageListener from './useAppMessageListener';
+
+describe('useAppMessageListener', () => {
+  const mockHandler = jest.fn();
+  // TODO: 이후 추가, 삭제되는 상수에 따라 적용
+  const mockProp: Parameters<typeof useAppMessageListener>[0] = {
+    targetType: 'foo',
+    handler: mockHandler,
+  };
+
+  beforeEach(() => {
+    Object.defineProperty(window, 'ReactNativeWebView', {
+      value: { postMessage: undefined },
+      writable: true,
+    });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('startListening과 stopListening을 반환해야 한다', () => {
+    const {
+      result: {
+        current: { startListening, stopListening },
+      },
+    } = renderHook(() => useAppMessageListener(mockProp));
+
+    expect(startListening).toBeDefined();
+    expect(stopListening).toBeDefined();
+  });
+
+  it('startListening 시 document와 window에 addEventListenr가 실행되어야 한다', () => {
+    jest.spyOn(document, 'addEventListener').mockImplementation();
+    jest.spyOn(window, 'addEventListener').mockImplementation();
+
+    const {
+      result: {
+        current: { startListening },
+      },
+    } = renderHook(() => useAppMessageListener(mockProp));
+
+    startListening();
+
+    expect(document.addEventListener).toBeCalled();
+    expect(window.addEventListener).toBeCalled();
+  });
+
+  it('stopListening 시 document와 window에 removeEventListener가 실행되어야 한다', () => {
+    jest.spyOn(document, 'removeEventListener').mockImplementation();
+    jest.spyOn(window, 'removeEventListener').mockImplementation();
+
+    const {
+      result: {
+        current: { stopListening },
+      },
+    } = renderHook(() => useAppMessageListener(mockProp));
+
+    stopListening();
+
+    expect(document.removeEventListener).toBeCalled();
+    expect(window.removeEventListener).toBeCalled();
+  });
+
+  it('targetType이 아닌 message 이벤트는 handler가 실행되면 안된다', () => {
+    const {
+      result: {
+        current: { startListening },
+      },
+    } = renderHook(() => useAppMessageListener(mockProp));
+
+    startListening();
+
+    const invalidTargetData = { type: 'not target type', data: 'foo' };
+    const stringifiedData = JSON.stringify(invalidTargetData);
+
+    fireEvent(window, new MessageEvent('message', { data: stringifiedData }));
+    expect(mockHandler).not.toBeCalled();
+  });
+
+  it('targetType이 같은 message 이벤트는 handler가 실행된다', () => {
+    const {
+      result: {
+        current: { startListening },
+      },
+    } = renderHook(() => useAppMessageListener(mockProp));
+
+    startListening();
+
+    const validTargetData = { type: mockProp.targetType, data: 'foo' };
+    const stringifiedData = JSON.stringify(validTargetData);
+
+    fireEvent(window, new MessageEvent('message', { data: stringifiedData }));
+    expect(mockHandler).toBeCalled();
+    expect(mockHandler).toBeCalledWith({ ...validTargetData, rest: {} });
+  });
+});
