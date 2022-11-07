194211b6c4bb538ff7fd063ddcac01828f15496c
eunddodi <eundppojjack@gmail.com>
2022-11-07 17:47:55 +0900

    feat: make mixpanelTrack fn get multiple args

--- src/lib/mixpanel.ts
+++ src/lib/mixpanel.ts
@@ -1,6 +1,6 @@
 export const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
 
-export const mixpanelTrack = (eventName: string, props: any) => {
+export const mixpanelTrack = (eventName: string, ...props: unknown[]) => {
   try {
     if (window.mixpanel) {
       window.mixpanel.track(eventName, props);
