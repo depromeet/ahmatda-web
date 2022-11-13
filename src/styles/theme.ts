31515a26d95488316c90713f761e2a6aff222883
eunddodi <eundppojjack@gmail.com>
2022-11-13 13:11:03 +0900

    feat: apply color scheme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,15 @@
 const theme = {
   colors: {
-    white: 'white',
+    white: '#FFFFFF',
+    gray1: '#F5F5F9',
+    gray2: '#E9E9EE',
+    gray3: '#C6C6CD',
+    gray4: '#9090A0',
+    gray5: '#626273',
+    gray6: '#464656',
+    black: '#212121',
+    primary: '#FF6C3E',
+    secondary: '#FF916F',
   },
 };
 
