c0dccfe2b58b7748c146259453bfe6a043fb35db
Hyesung Oh <haesungoh414@gmail.com>
2022-11-14 22:26:20 +0900

    chore: 테스트 페이지 적용

---
+++ src/pages/test/index.page.tsx
@@ -0,0 +1,30 @@
+import Button from '@/components/button/Button';
+import ContainedButton from '@/components/button/ContainedButton';
+import LabelButton from '@/components/button/LabelButton';
+
+const Test = () => {
+  return (
+    <div>
+      <Button>테스트 버튼</Button>
+
+      <ContainedButton size="medium">컨테인 버튼 medium</ContainedButton>
+      <ContainedButton size="medium" disabled>
+        컨테인 버튼 medium
+      </ContainedButton>
+
+      <ContainedButton size="large">컨테인 버튼 large</ContainedButton>
+      <ContainedButton size="large" disabled>
+        컨테인 버튼 large
+      </ContainedButton>
+
+      <LabelButton>라벨 버튼 small</LabelButton>
+      <LabelButton disabled>라벨 버튼 small</LabelButton>
+      <LabelButton size="large">라벨 버튼 large</LabelButton>
+      <LabelButton size="large" disabled>
+        라벨 버튼 large
+      </LabelButton>
+    </div>
+  );
+};
+
+export default Test;
