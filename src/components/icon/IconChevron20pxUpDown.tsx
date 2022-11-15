fdcec9e83be67de847ae6992f88279585b6c1a68
Hyesung Oh <haesungoh414@gmail.com>
2022-11-15 11:17:06 +0900

    feat: chevron 아이콘 개발

---
+++ src/components/icon/IconChevron20pxUpDown.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+
+import Svg, { Props as SvgProps } from '../svg/Svg';
+
+type Direction = 'up' | 'down';
+
+const DIRECTION_DEGREE: { [K in Direction]: number } = { up: 0, down: 180 };
+
+interface Props extends SvgProps {
+  direction?: Direction;
+}
+
+const IconChevron20pxUpDown = ({ direction = 'up', ...rest }: Props) => {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <StyledSvg degree={DIRECTION_DEGREE[direction]} size={20} viewBox="0 0 20 20" {...rest}>
+      <path
+        d="M14 11.917L10.3536 8.27059C10.1583 8.07532 9.84175 8.07532 9.64649 8.27059L6.00008 11.917"
+        stroke="#9090A0"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </StyledSvg>
+  );
+};
+
+export default IconChevron20pxUpDown;
+
+const StyledSvg = styled(Svg)<{ degree: number }>`
+  transform: rotate(${({ degree }) => degree}deg);
+`;
---
+++ src/components/icon/IconChevron24pxUpDown.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+
+import Svg, { Props as SvgProps } from '../svg/Svg';
+
+type Direction = 'up' | 'down';
+
+const DIRECTION_DEGREE: { [K in Direction]: number } = { up: 0, down: 180 };
+
+interface Props extends SvgProps {
+  direction?: Direction;
+}
+
+const IconChevron24pxUpDown = ({ direction = 'up', ...rest }: Props) => {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <StyledSvg degree={DIRECTION_DEGREE[direction]} {...rest}>
+      <path
+        d="M18 14.8999L12.3536 9.25346C12.1583 9.05819 11.8417 9.05819 11.6464 9.25345L6 14.8999"
+        stroke="#9090A0"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </StyledSvg>
+  );
+};
+
+export default IconChevron24pxUpDown;
+
+const StyledSvg = styled(Svg)<{ degree: number }>`
+  transform: rotate(${({ degree }) => degree}deg);
+`;
