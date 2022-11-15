531f3ff1adc9f4eca31c910c69bf5d8786dd72ca
Hyesung Oh <haesungoh414@gmail.com>
2022-11-15 23:48:19 +0900

    chore: 예시 페이지에 bottom sheet 적용

--- src/pages/test/index.page.tsx
+++ src/pages/test/index.page.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+import styled from '@emotion/styled';
+
 import Button from '@/components/button/Button';
 import ContainedButton from '@/components/button/ContainedButton';
 import LabelButton from '@/components/button/LabelButton';
 
+const BottomSheet = dynamic(() => import('@/components/portal/BottomSheet'));
+
 const Test = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <div>
+      <Heading>버튼</Heading>
       <Button>테스트 버튼</Button>
 
       <ContainedButton size="medium">컨테인 버튼 medium</ContainedButton>
@@ -23,8 +32,19 @@ const Test = () => {
       <LabelButton size="large" disabled>
         라벨 버튼 large
       </LabelButton>
+
+      <Heading>bottom sheet</Heading>
+
+      <Button onClick={() => setIsOpen((prev) => !prev)}>bottom sheet 열기</Button>
+      <BottomSheet setToClose={() => setIsOpen(false)} isShowing={isOpen}>
+        <div>어쩌구저쩌구</div>
+      </BottomSheet>
     </div>
   );
 };
 
 export default Test;
+
+const Heading = styled.h2`
+  border-top: solid 1px black;
+`;
