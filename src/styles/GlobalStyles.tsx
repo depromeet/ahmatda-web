cea72ef280f6031e9d5b707a07a4977df62b39df
Hyesung Oh <haesungoh414@gmail.com>
2022-11-16 00:05:36 +0900

    refactor: global style 중복 styled 제거

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -9,7 +9,6 @@ export const setGlobalStyles = css`
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
       'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
       sans-serif;
-    box-sizing: border-box;
   }
 
   a {
