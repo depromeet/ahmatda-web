c6f49dde3f2322593d3c796689c6194038a202d7
Hyesung Oh <haesungoh414@gmail.com>
2022-11-19 00:20:26 +0900

    chore: test 페이지 적용

--- src/pages/test/index.page.tsx
+++ src/pages/test/index.page.tsx
@@ -1,29 +1,33 @@
 import Button from '@/components/button/Button';
 import ContainedButton from '@/components/button/ContainedButton';
 import LabelButton from '@/components/button/LabelButton';
+import AppBar from '@/components/navigation/AppBar';
 
 const Test = () => {
   return (
-    <div>
-      <Button>테스트 버튼</Button>
+    <>
+      <AppBar title="test" />
+      <div>
+        <Button>테스트 버튼</Button>
 
-      <ContainedButton size="medium">컨테인 버튼 medium</ContainedButton>
-      <ContainedButton size="medium" disabled>
-        컨테인 버튼 medium
-      </ContainedButton>
+        <ContainedButton size="medium">컨테인 버튼 medium</ContainedButton>
+        <ContainedButton size="medium" disabled>
+          컨테인 버튼 medium
+        </ContainedButton>
 
-      <ContainedButton size="large">컨테인 버튼 large</ContainedButton>
-      <ContainedButton size="large" disabled>
-        컨테인 버튼 large
-      </ContainedButton>
+        <ContainedButton size="large">컨테인 버튼 large</ContainedButton>
+        <ContainedButton size="large" disabled>
+          컨테인 버튼 large
+        </ContainedButton>
 
-      <LabelButton>라벨 버튼 small</LabelButton>
-      <LabelButton disabled>라벨 버튼 small</LabelButton>
-      <LabelButton size="large">라벨 버튼 large</LabelButton>
-      <LabelButton size="large" disabled>
-        라벨 버튼 large
-      </LabelButton>
-    </div>
+        <LabelButton>라벨 버튼 small</LabelButton>
+        <LabelButton disabled>라벨 버튼 small</LabelButton>
+        <LabelButton size="large">라벨 버튼 large</LabelButton>
+        <LabelButton size="large" disabled>
+          라벨 버튼 large
+        </LabelButton>
+      </div>
+    </>
   );
 };
 
