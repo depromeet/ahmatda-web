e8ee0494a01a1c4e47419f91df846f5b6cad5a3a
Hyesung Oh <haesungoh414@gmail.com>
2022-11-18 23:25:46 +0900

    feat: icon-button 컴포넌트 개발

---
+++ src/components/button/IconButton.tsx
@@ -0,0 +1,34 @@
+import { ComponentProps, FC } from 'react';
+import styled from '@emotion/styled';
+
+import Button from './Button';
+
+type Props = ComponentProps<typeof Button>;
+
+const IconButton: FC<Props> = ({ children, ...rest }) => {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <StyledButton {...rest}>{children}</StyledButton>;
+};
+
+export default IconButton;
+
+const StyledButton = styled(Button)(
+  {
+    // !: #39 merge 이후 정리
+    boxSizing: 'border-box',
+    borderRadius: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '8px',
+    width: '40px',
+    height: '40px',
+    '&:hover': {
+      backgroundColor: 'inherit',
+    },
+  },
+  ({ theme }) => ({
+    backgroundColor: theme.colors.white,
+    color: theme.colors.gray4,
+  }),
+);
