8489f22e0f6ffb63c5f101d44aabbbe2384acf15
hansol <dundunki@naver.com>
2022-11-18 23:25:40 +0900

    fix: 테스트 코드 삭제

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,13 +1,5 @@
-import ToggleSwitch from '@/components/toggle/ToggleSwitch'
-import ToggleSwitch2 from '@/components/toggle/ToggleSwitch2'
-
 const HomePage = () => {
-  return <div>HomePage
-
-  <ToggleSwitch onChange={()=>alert("g하하")}/>
-  {/* <ToggleSwitch2 /> */}
-
-  </div>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
