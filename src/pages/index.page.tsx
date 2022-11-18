a2d1ab66927a00807bb778770bd4c4e097756230
hansol <dundunki@naver.com>
2022-11-18 22:56:41 +0900

    add: add toggle component

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,13 @@
+import ToggleSwitch from '@/components/toggle/ToggleSwitch'
+import ToggleSwitch2 from '@/components/toggle/ToggleSwitch2'
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div>HomePage
+
+  <ToggleSwitch onChange={()=>alert("g하하")}/>
+  {/* <ToggleSwitch2 /> */}
+
+  </div>;
 };
 
 export default HomePage;
