edcbf2ba76bd297a7d05d06163c460cfd15d625c
Hyesung Oh <haesungoh414@gmail.com>
2022-11-20 19:17:36 +0900

    feat: home, template route에 bottom-navigation 적용

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,25 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import { ReactElement } from 'react';
+
+import { NextPageWithLayout } from './_app.page';
+
+import BottomNavigation from '@/components/navigation/BottomNavigation';
+
+const HomePage: NextPageWithLayout = () => {
+  return (
+    <div>
+      <div style={{ height: '90vh', backgroundColor: 'blue' }} />
+      <div style={{ height: '10vh', backgroundColor: 'red' }} />
+    </div>
+  );
+};
+
+HomePage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <>
+      {page}
+      <BottomNavigation />
+    </>
+  );
 };
 
 export default HomePage;
