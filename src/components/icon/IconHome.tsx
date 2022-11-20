1f0b385b35f781be3af70f4da25a7619623d0ec2
Hyesung Oh <haesungoh414@gmail.com>
2022-11-20 19:15:53 +0900

    feat: home, search icon transition 적용

--- src/components/icon/IconHome.tsx
+++ src/components/icon/IconHome.tsx
@@ -17,12 +17,21 @@ const HOME_COLOR = (isAct: boolean): HomeColor =>
 
 const IconHome = ({ isAct = false, ...rest }: Props) => (
   <Svg {...rest}>
-    <rect x="1.5" y="1.5" width="21" height="21" rx="5" fill={HOME_COLOR(isAct).rect} />
+    <rect
+      x="1.5"
+      y="1.5"
+      width="21"
+      height="21"
+      rx="5"
+      fill={HOME_COLOR(isAct).rect}
+      style={{ transition: 'fill .3s' }}
+    />
     <path
       d="M16.125 9.46191L11.4017 14.1852C11.2064 14.3805 10.8898 14.3805 10.6946 14.1852L7.875 11.3657"
       stroke={HOME_COLOR(isAct).path}
       strokeWidth="2"
       strokeLinecap="round"
+      style={{ transition: 'stroke .3s' }}
     />
   </Svg>
 );
