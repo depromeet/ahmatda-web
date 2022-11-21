3e236f6479d2bc69ffff02765b2e263856bf4a7e
eunddodi <eundppojjack@gmail.com>
2022-11-21 11:50:19 +0900

    refactor: implement Checkbox

--- src/components/checkbox/Checkbox.test.tsx
+++ src/components/checkbox/Checkbox.test.tsx
@@ -5,17 +5,12 @@ import Checkbox from './Checkbox';
 import customRender from '@/test/customRender';
 
 describe('checkbox', () => {
-  it('클릭할 때마다 onCheck와 onUncheck가 번갈아 동작해야 한다', () => {
-    const onCheckMock = jest.fn();
-    const onUncheckMock = jest.fn();
-    customRender(<Checkbox onCheck={onCheckMock} onUncheck={onUncheckMock} />);
+  it('클릭시마다 onToggle이 작동한다.', () => {
+    const onToggleMock = jest.fn();
+    customRender(<Checkbox onToggle={onToggleMock} />);
 
-    fireEvent.click(screen.getByRole('checkbox'));
-
-    fireEvent.click(screen.getByRole('checkbox'));
-    expect(onUncheckMock).toBeCalledTimes(1);
-
-    fireEvent.click(screen.getByRole('checkbox'));
-    expect(onCheckMock).toBeCalledTimes(2);
+    fireEvent.click(screen.getByTestId('checkbox'));
+    fireEvent.click(screen.getByTestId('checkbox'));
+    expect(onToggleMock).toHaveBeenCalledTimes(2);
   });
 });
