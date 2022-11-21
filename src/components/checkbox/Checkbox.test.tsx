7ffcb13aa59decfdfb2482a19c4d6822bbc03bdf
eunddodi <eundppojjack@gmail.com>
2022-11-21 14:59:19 +0900

    test: checkbox with text

--- src/components/checkbox/Checkbox.test.tsx
+++ src/components/checkbox/Checkbox.test.tsx
@@ -1,8 +1,11 @@
 import { fireEvent, screen } from '@testing-library/react';
 
 import Checkbox from './Checkbox';
+import CheckboxWithText from './CheckboxWithText';
 
 import customRender from '@/test/customRender';
+
+const CHECKBOX_TEXT = 'test';
 
 describe('checkbox', () => {
   it('클릭시마다 onToggle이 작동한다.', () => {
@@ -12,5 +15,10 @@ describe('checkbox', () => {
     fireEvent.click(screen.getByTestId('checkbox'));
     fireEvent.click(screen.getByTestId('checkbox'));
     expect(onToggleMock).toHaveBeenCalledTimes(2);
+  });
+
+  it('children을 렌더링한다.', () => {
+    customRender(<CheckboxWithText>test</CheckboxWithText>);
+    expect(screen.getByText(CHECKBOX_TEXT)).toBeInTheDocument();
   });
 });
