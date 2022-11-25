f4a4cb6184eb967d729681f44ba3d63bde134227
hansol <dundunki@naver.com>
2022-11-25 18:27:15 +0900

    feat: Item 컴포넌트 개발

---
+++ src/components/item/Item.tsx
@@ -0,0 +1,60 @@
+import styled from '@emotion/styled';
+
+interface ItemProps extends React.HTMLAttributes<HTMLInputElement> {
+  type?: 'radio' | 'checkbox';
+  name?: string;
+  label?: string;
+  emjCode?: string | undefined;
+  labelSize?: 'small' | 'large';
+}
+
+const Item = ({ label, emjCode, labelSize = 'small', ...rest }: ItemProps) => {
+  return (
+    <>
+      <ItemInput id={label} emjCode={emjCode} {...rest} data-testid="item-input" />
+      <ItemLabel htmlFor={label} emjCode={emjCode} labelSize={labelSize} data-testid="item-label">
+        {emjCode && <span data-testid="item-emoji">{emjCode}</span>}
+        <LabelText>{label}</LabelText>
+      </ItemLabel>
+    </>
+  );
+};
+
+export default Item;
+
+const ItemLabel = styled.label<ItemProps>(
+  {
+    padding: '8px 16px',
+    borderRadius: '8px',
+  },
+  ({ theme, labelSize }) => ({
+    backgroundColor: theme.colors.gray1,
+
+    ...(labelSize === 'large' && {
+      ...theme.typographies.button1,
+    }),
+    ...(labelSize === 'small' && {
+      ...theme.typographies.button2,
+    }),
+  }),
+);
+
+const ItemInput = styled.input<ItemProps>(
+  {
+    height: '0',
+    width: '0',
+    visibility: 'hidden',
+  },
+  ({ theme, emjCode }) => ({
+    color: theme.colors.black,
+
+    '&:checked + label': {
+      color: theme.colors.white,
+      backgroundColor: emjCode ? theme.colors.black : theme.colors.gray4,
+    },
+  }),
+);
+
+const LabelText = styled.span({
+  marginLeft: '8px',
+});
