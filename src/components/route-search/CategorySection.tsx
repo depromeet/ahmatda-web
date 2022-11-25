9ceaa110684d63e6954a658b355ff91936060c1d
eunddodi <eundppojjack@gmail.com>
2022-11-25 13:58:50 +0900

    feat: 탐색 탭 마크업

---
+++ src/components/route-search/CategorySection.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Chip from '../chip/Chip';
+import styled from '@emotion/styled';
+
+function CategorySection() {
+  return (
+    <Wrapper>
+      {CATEGORY_DUMMY.map((name) => (
+        <Chip label={name} />
+      ))}
+    </Wrapper>
+  );
+}
+
+export default CategorySection;
+
+const CATEGORY_DUMMY = ['물놀이', '디프만', '호캉스', '일상', '운동', '유럽여행'];
+
+const Wrapper = styled.div`
+  overflow: auto;
+  white-space: nowrap;
+`;
---
+++ src/components/route-search/ListRequestSection.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import Button from '../button/Button';
+
+function ListRequestSection() {
+  return (
+    <div>
+      <MainTitle>찾으시는 리스트가 없나요?</MainTitle>
+      <SubTitle>필요한 상황의 리스트를 말씀해 주세요.</SubTitle>
+      <Button>리스트 요청하기</Button>
+    </div>
+  );
+}
+
+export default ListRequestSection;
+
+const MainTitle = styled.p`
+  ${({ theme }) => ({ ...theme.typographies.title2, color: theme.colors.black })};
+  margin-bottom: 4px;
+`;
+const SubTitle = styled.p`
+  ${({ theme }) => ({ ...theme.typographies.body1, color: theme.colors.gray4 })};
+  margin-bottom: 16px;
+`;
