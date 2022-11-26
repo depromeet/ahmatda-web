2df82511f15d36268fc558baac1f082abf24fc7b
hansol <dundunki@naver.com>
2022-11-26 15:22:04 +0900

    fix: 스크롤 처리 추가

--- src/components/route-onboard/ButtonSection.tsx
+++ src/components/route-onboard/ButtonSection.tsx
@@ -4,15 +4,22 @@ import styled from '@emotion/styled';
 import ContainedButton from '@/components/button/ContainedButton';
 
 const ButtonSection = ({ children }: ComponentProps<typeof ContainedButton>) => {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <>
+      <Wrapper>{children}</Wrapper>
+      <ScrollableDiv />
+    </>
+  );
 };
 
 export default ButtonSection;
 
+const HEIGHT = '80px';
+
 const Wrapper = styled.section(
   {
     width: '100%',
-    height: '80px',
+    height: HEIGHT,
     position: 'fixed',
     bottom: 0,
     left: 0,
@@ -21,3 +28,5 @@ const Wrapper = styled.section(
   },
   ({ theme }) => ({ backgroundColor: theme.colors.white }),
 );
+
+const ScrollableDiv = styled.div({ height: HEIGHT, appearance: 'none' });
