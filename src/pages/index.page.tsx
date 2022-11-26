7528838c34cf0a8c102922878bdd2f56704f4e1e
Hyesung Oh <haesungoh414@gmail.com>
2022-11-26 21:35:45 +0900

    feat: category-section 개발 및 적용

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -4,10 +4,16 @@ import { NextPageWithLayout } from './_app.page';
 
 import BottomNavigation from '@/components/navigation/BottomNavigation';
 import DefaultAppBar from '@/components/navigation/DefaultAppBar';
+import CategorySection from '@/components/route-home/CategorySection';
 import RecommendSection from '@/components/route-home/RecommendSection';
 
 const HomePage: NextPageWithLayout = () => {
-  return <RecommendSection />;
+  return (
+    <>
+      <CategorySection />
+      <RecommendSection />
+    </>
+  );
 };
 
 HomePage.getLayout = function getLayout(page: ReactElement) {
