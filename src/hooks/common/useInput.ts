ec710cdf05481f54f464d4eb97e724071452307e
Hyesung Oh <haesungoh414@gmail.com>
2022-11-29 11:33:45 +0900

    refactor: 간결화 및 type 제한

--- src/hooks/common/useInput.ts
+++ src/hooks/common/useInput.ts
@@ -1,7 +1,11 @@
 import { ChangeEvent, Dispatch, SetStateAction, useCallback, useMemo, useState } from 'react';
 import debounce from 'lodash/debounce';
 
-interface DebounceOption {
+interface Props<T> {
+  /**
+   * value, debouncedValue의 초기값이에요
+   */
+  initialValue: T;
   /**
    * debounce를 사용할 것인지 정의해요
    *
@@ -14,22 +18,10 @@ interface DebounceOption {
    * @default 150
    */
   debounceTimeout?: number;
-}
-
-interface PropsWhenString extends DebounceOption {
-  /**
-   * value, debouncedValue의 초기값이에요
-   */
-  initialValue: string;
-}
-
-interface PropsWhenGeneric<T> extends DebounceOption {
-  /**
-   * value, debouncedValue의 초기값이에요
-   */
-  initialValue: T;
   /**
    * onChange의 value의 타입을 변환해 setStating할 parser에요
+   *
+   * type이 string이 아닐 시 `required` 해요
    *
    * @example
    * ```tsx
@@ -40,45 +32,37 @@ interface PropsWhenGeneric<T> extends DebounceOption {
   parser?: (value: string) => T;
 }
 
-type Props<T> = PropsWhenString & PropsWhenGeneric<T>;
+type PropsWhenNotString<T> = WithRequired<Props<T>, 'parser'>;
 
 interface Return<T> {
   value: T;
   setValue: Dispatch<SetStateAction<T>>;
-  /**
-   * debounce에 의해 갱신된 value에요
-   *
-   * `useDebounce`가 `true`로 설정되어 있어야 갱신돼요
-   */
   debouncedValue: T;
-  /**
-   * input의 onChange 핸들러에요
-   *
-   * @example
-   * ```tsx
-   * <input value={value} onChange={onChange} />
-   * ```
-   */
   onChange: (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-function useInput({ initialValue, useDebounce, debounceTimeout }: PropsWhenString): Return<string>;
+function useInput({ initialValue, useDebounce, debounceTimeout, parser }: Props<string>): Return<string>;
 
-function useInput<T>({ initialValue, useDebounce, debounceTimeout, parser }: PropsWhenGeneric<T>): Return<T>;
+function useInput<T>({ initialValue, useDebounce, debounceTimeout, parser }: PropsWhenNotString<T>): Return<T>;
 
-function useInput<T>({ initialValue, useDebounce = false, debounceTimeout = 150, parser }: Props<T>) {
-  const [value, setValue] = useState<string | T>(initialValue);
-  const [debouncedValue, setDebouncedValue] = useState<string | T>(initialValue);
+function useInput<T extends string | number | boolean>({
+  initialValue,
+  useDebounce = false,
+  debounceTimeout = 150,
+  parser,
+}: Props<T>) {
+  const [value, setValue] = useState<T>(initialValue);
+  const [debouncedValue, setDebouncedValue] = useState<T>(initialValue);
 
   const handleDebounceValue = useMemo(
     () =>
-      debounce((changedValue: string | T) => {
+      debounce((changedValue: T) => {
         setDebouncedValue(changedValue);
       }, debounceTimeout),
     [],
   );
 
-  const handleChangedValue = useCallback((changedValue: string | T) => {
+  const handleChangedValue = useCallback((changedValue: T) => {
     setValue(changedValue);
     if (useDebounce) handleDebounceValue(changedValue);
   }, []);
@@ -89,43 +73,10 @@ function useInput<T>({ initialValue, useDebounce = false, debounceTimeout = 150,
       return;
     }
 
-    handleChangedValue(e.target.value);
+    handleChangedValue(e.target.value as T);
   }, []);
 
   return { value, setValue, debouncedValue, onChange };
 }
-// const useInput = <T>({
-//   initialValue,
-//   useDebounce = false,
-//   debounceTimeout = 150,
-//   parser,
-// }: PropsWhenString & PropsWhenGeneric<T>) => {
-//   const [value, setValue] = useState<string | T>(initialValue);
-//   const [debouncedValue, setDebouncedValue] = useState<string | T>(initialValue);
-
-//   const handleDebounceValue = useMemo(
-//     () =>
-//       debounce((changedValue: string | T) => {
-//         setDebouncedValue(changedValue);
-//       }, debounceTimeout),
-//     [],
-//   );
-
-//   const handleChangedValue = useCallback((changedValue: string | T) => {
-//     setValue(changedValue);
-//     if (useDebounce) handleDebounceValue(changedValue);
-//   }, []);
-
-//   const onChange = useCallback((e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
-//     if (parser) {
-//       handleChangedValue(parser(e.target.value));
-//       return;
-//     }
-
-//     handleChangedValue(e.target.value);
-//   }, []);
-
-//   return { value, setValue, debouncedValue, onChange };
-// };
 
 export default useInput;
