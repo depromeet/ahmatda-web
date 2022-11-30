7fc7c0cdceb3787564312a4fa14cd024682a6a86
eunddodi <eundppojjack@gmail.com>
2022-12-01 01:04:41 +0900

    fix: lint

--- src/pages/test/index.page.tsx
+++ src/pages/test/index.page.tsx
@@ -222,24 +222,6 @@ const TestDiv = styled.div`
   background-color: green;
 `;
 
-const CardBackground = styled.div`
-  background-color: lightgray;
-`;
-
-const SEARCH_CARD_DUMMY_TITLE = '디프만 UT 준비물';
-const SEARCH_CARD_DUMMY_DATA = [
-  { name: '구민규', id: '1' },
-  { name: '김민걸', id: '2' },
-  { name: '명수찬', id: '3' },
-  { name: '박한솔', id: '4' },
-  { name: '오혜성', id: '5' },
-  { name: '윤가빈', id: '6' },
-  { name: '이영희', id: '7' },
-  { name: '이은지', id: '8' },
-  { name: '이종원', id: '9' },
-  { name: '조성민', id: '10' },
-];
-
 const ItemBackground = styled.div`
   background-color: lightgray;
 `;
