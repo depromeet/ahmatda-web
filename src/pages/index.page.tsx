54571b401c8a704d8bec598bf90ef364be9b8fb3
eunddodi <eundppojjack@gmail.com>
2022-12-02 02:26:09 +0900

    feat: 리스트 설정 bottomsheet 마크업

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -7,10 +7,14 @@ import BottomNavigation from '@/components/navigation/BottomNavigation';
 import DefaultAppBar from '@/components/navigation/DefaultAppBar';
 import CategorySection from '@/components/route-home/CategorySection';
 import EmptyCard from '@/components/route-home/EmptyCard';
+import ListSettingBottomSheet from '@/components/route-home/ListSettingBottomSheet';
 import RecommendSection from '@/components/route-home/RecommendSection';
 
 const HomePage: NextPageWithLayout = () => {
   const [carouselWrapper, setCarouselWrapper] = useState<HTMLDivElement | null>(null);
+
+  // TODO: Home Card 컴포넌트 개발 시 해당 컴포넌트 내부로 이동 예정
+  const [isShowing, setIsShowing] = useState(false);
 
   return (
     <>
@@ -25,6 +29,20 @@ const HomePage: NextPageWithLayout = () => {
       <Carousel.Indicator carouselWrapper={carouselWrapper} />
 
       <RecommendSection />
+      <button
+        onClick={() => {
+          setIsShowing(true);
+        }}
+        type="button"
+      >
+        리스트 설정 BottomSheet 열기
+      </button>
+      <ListSettingBottomSheet
+        isShowing={isShowing}
+        setToClose={() => {
+          setIsShowing(false);
+        }}
+      />
     </>
   );
 };
