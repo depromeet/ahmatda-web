7f3898a39680442356709b6e7512f41f1503020e
Hyesung Oh <haesungoh414@gmail.com>
2022-12-06 16:46:09 +0900

    feat: axios instance 작성

---
+++ src/lib/api.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+import { isProd } from '@/utils/utils';
+
+// TODO: API URL 대응
+const PRODUCTION_API_URL = '';
+const DEVELOPMENT_API_URL = '';
+
+const instance = axios.create({
+  baseURL: isProd(process.env.NODE_ENV) ? PRODUCTION_API_URL : DEVELOPMENT_API_URL,
+});
+
+const interceptorResponseFulfilled = (response: AxiosResponse) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response.data;
+  }
+
+  return Promise.reject(response.data);
+};
+
+const interceptorResponseRejected = (error: AxiosError<{ error: string }>) => {
+  return Promise.reject(new Error(error.response?.data.error));
+};
+
+instance.interceptors.response.use(interceptorResponseFulfilled, interceptorResponseRejected);
+
+export function get<T>(...args: Parameters<typeof instance.get>) {
+  return instance.get<T, T>(...args);
+}
+
+export function post<T>(...args: Parameters<typeof instance.post>) {
+  return instance.post<T, T>(...args);
+}
+
+export function put<T>(...args: Parameters<typeof instance.put>) {
+  return instance.put<T, T>(...args);
+}
+
+export function patch<T>(...args: Parameters<typeof instance.patch>) {
+  return instance.patch<T, T>(...args);
+}
+
+export function del<T>(...args: Parameters<typeof instance.delete>) {
+  return instance.delete<T, T>(...args);
+}
