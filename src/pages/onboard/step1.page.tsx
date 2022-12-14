d60aec3b9c500433b7ab7d9e618e3cde75ad6f5a
hyesung oh <haesungoh414@gmail.com>
2022-12-15 00:10:25 +0900

    onboard route에 WhiteBackgroundGlobalStyle 사용 (#169)

--- src/pages/onboard/step1.page.tsx
+++ src/pages/onboard/step1.page.tsx
@@ -10,6 +10,7 @@ import TitleSection from '@/components/route-onboard/TitleSection';
 import { staggerOne } from '@/constants/motions';
 import { post } from '@/lib/api';
 import userTokenState from '@/store/localStorage/userToken';
+import { WhiteBackgroundGlobalStyles } from '@/styles/GlobalStyles';
 
 const Step1 = () => {
   const onSubmit = (e: React.FormEvent) => {
@@ -32,45 +33,48 @@ const Step1 = () => {
   };
 
   return (
-    <Wrapper>
-      <TitleSection
-        title={
-          <>
-            만나서 반가워요.
-            <br />
-            어떤 상황에서 소지품을
-            <br />
-            관리 받고 싶으신가요?
-          </>
-        }
-        subTitle={<>소지품 관리가 가장 필요한 상황을 1개 골라 주세요.</>}
-      />
+    <>
+      <WhiteBackgroundGlobalStyles />
+      <Wrapper>
+        <TitleSection
+          title={
+            <>
+              만나서 반가워요.
+              <br />
+              어떤 상황에서 소지품을
+              <br />
+              관리 받고 싶으신가요?
+            </>
+          }
+          subTitle={<>소지품 관리가 가장 필요한 상황을 1개 골라 주세요.</>}
+        />
 
-      <button type="button" onClick={testTokenAndSave}>
-        테스트 토큰 발급 및 저장
-      </button>
+        <button type="button" onClick={testTokenAndSave}>
+          테스트 토큰 발급 및 저장
+        </button>
 
-      <form onSubmit={onSubmit}>
-        <SelectSection variants={staggerOne} initial="initial" animate="animate" exit="exit">
-          <Item
-            type="radio"
-            name="category"
-            value="daily"
-            label="일상"
-            emjCode="&#x1F4BC;"
-            labelSize="large"
-            defaultChecked
-          />
-          <Item type="radio" name="category" value="exercise" label="운동" emjCode="&#x1F4AA;" labelSize="large" />
-          <Item type="radio" name="category" value="traval" label="여행" emjCode="&#x2708;" labelSize="large" />
-        </SelectSection>
-        <ButtonSection>
-          <ContainedButton type="submit" size="large">
-            다음
-          </ContainedButton>
-        </ButtonSection>
-      </form>
-    </Wrapper>
+        <form onSubmit={onSubmit}>
+          <SelectSection variants={staggerOne} initial="initial" animate="animate" exit="exit">
+            <Item
+              type="radio"
+              name="category"
+              value="daily"
+              label="일상"
+              emjCode="&#x1F4BC;"
+              labelSize="large"
+              defaultChecked
+            />
+            <Item type="radio" name="category" value="exercise" label="운동" emjCode="&#x1F4AA;" labelSize="large" />
+            <Item type="radio" name="category" value="traval" label="여행" emjCode="&#x2708;" labelSize="large" />
+          </SelectSection>
+          <ButtonSection>
+            <ContainedButton type="submit" size="large">
+              다음
+            </ContainedButton>
+          </ButtonSection>
+        </form>
+      </Wrapper>
+    </>
   );
 };
 
--- src/pages/onboard/step2.page.tsx
+++ src/pages/onboard/step2.page.tsx
@@ -6,6 +6,7 @@ import Item from '@/components/item/Item';
 import ButtonSection from '@/components/route-onboard/ButtonSection';
 import TitleSection from '@/components/route-onboard/TitleSection';
 import { staggerOne } from '@/constants/motions';
+import { WhiteBackgroundGlobalStyles } from '@/styles/GlobalStyles';
 
 const TITLE = {
   daily: {
@@ -31,36 +32,39 @@ const Step2 = () => {
   };
 
   return (
-    <Wrapper>
-      <TitleSection title={TITLE.daily.title} subTitle={TITLE.daily.subTitle} />
-      <form onSubmit={onSubmit}>
-        <SelectSection variants={staggerOne} initial="initial" animate="animate" exit="exit">
-          <Item name="item" label="이어폰" emjCode="&#x1F3A7;" labelSize="small" />
-          <Item name="item" label="노트북" emjCode="&#x1F4BB;" labelSize="small" />
-          <Item name="item" label="충전기" emjCode="&#x1F50C;" labelSize="small" />
-          <Item name="item" label="보조배터리" emjCode="&#x1F50B;" labelSize="small" />
-          <Item name="item" label="시계" emjCode="&#x231A;" labelSize="small" />
-          <Item name="item" label="지갑" emjCode="&#x1F45B;" labelSize="small" />
-          <Item name="item" label="카드" emjCode="&#x1F4B3;" labelSize="small" />
-          <Item name="item" label="열쇠" emjCode="&#x1F511;" labelSize="small" />
-          <Item name="item" label="마스크" emjCode="&#x1F637;" labelSize="small" />
-          <Item name="item" label="안경" emjCode="&#x1F453;" labelSize="small" />
-          <Item name="item" label="향수" emjCode="&#x1F4A8;" labelSize="small" />
-          <Item name="item" label="파우치" emjCode="&#x1F45D;" labelSize="small" />
-          <Item name="item" label="인공눈물" emjCode="&#x1F4A7;" labelSize="small" />
-          <Item name="item" label="약" emjCode="&#x1F48A;" labelSize="small" />
-          <Item name="item" label="양치도구" emjCode="&#x1FAA5;" labelSize="small" />
-          <Item name="item" label="우산" emjCode="&#x1F302;" labelSize="small" />
-          <Item name="item" label="텀블러" emjCode="&#x1F964;" labelSize="small" />
-          <Item name="item" label="휴지" emjCode="&#x1F9FB;" labelSize="small" />
-        </SelectSection>
-        <ButtonSection>
-          <ContainedButton type="submit" size="large">
-            다음
-          </ContainedButton>
-        </ButtonSection>
-      </form>
-    </Wrapper>
+    <>
+      <WhiteBackgroundGlobalStyles />
+      <Wrapper>
+        <TitleSection title={TITLE.daily.title} subTitle={TITLE.daily.subTitle} />
+        <form onSubmit={onSubmit}>
+          <SelectSection variants={staggerOne} initial="initial" animate="animate" exit="exit">
+            <Item name="item" label="이어폰" emjCode="&#x1F3A7;" labelSize="small" />
+            <Item name="item" label="노트북" emjCode="&#x1F4BB;" labelSize="small" />
+            <Item name="item" label="충전기" emjCode="&#x1F50C;" labelSize="small" />
+            <Item name="item" label="보조배터리" emjCode="&#x1F50B;" labelSize="small" />
+            <Item name="item" label="시계" emjCode="&#x231A;" labelSize="small" />
+            <Item name="item" label="지갑" emjCode="&#x1F45B;" labelSize="small" />
+            <Item name="item" label="카드" emjCode="&#x1F4B3;" labelSize="small" />
+            <Item name="item" label="열쇠" emjCode="&#x1F511;" labelSize="small" />
+            <Item name="item" label="마스크" emjCode="&#x1F637;" labelSize="small" />
+            <Item name="item" label="안경" emjCode="&#x1F453;" labelSize="small" />
+            <Item name="item" label="향수" emjCode="&#x1F4A8;" labelSize="small" />
+            <Item name="item" label="파우치" emjCode="&#x1F45D;" labelSize="small" />
+            <Item name="item" label="인공눈물" emjCode="&#x1F4A7;" labelSize="small" />
+            <Item name="item" label="약" emjCode="&#x1F48A;" labelSize="small" />
+            <Item name="item" label="양치도구" emjCode="&#x1FAA5;" labelSize="small" />
+            <Item name="item" label="우산" emjCode="&#x1F302;" labelSize="small" />
+            <Item name="item" label="텀블러" emjCode="&#x1F964;" labelSize="small" />
+            <Item name="item" label="휴지" emjCode="&#x1F9FB;" labelSize="small" />
+          </SelectSection>
+          <ButtonSection>
+            <ContainedButton type="submit" size="large">
+              다음
+            </ContainedButton>
+          </ButtonSection>
+        </form>
+      </Wrapper>
+    </>
   );
 };
 
