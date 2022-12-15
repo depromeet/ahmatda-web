0a81409f4ac461fc7a2e6fe0c6873bcb3b91bd2a
eunddodi <87167786+eunddodi@users.noreply.github.com>
2022-12-15 18:01:11 +0900

    알림 설정 페이지 마크업, 정보 페이지 마크업 (#175)
    Co-authored-by: eunddodi <eundppojjack@gmail.com>

--- src/pages/information/index.page.tsx
+++ src/pages/information/index.page.tsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled';
+
 import AppBar from '@/components/navigation/AppBar';
 import { WhiteBackgroundGlobalStyles } from '@/styles/GlobalStyles';
 
@@ -8,9 +10,39 @@ const Information = () => {
 
       <main>
         <AppBar title="정보" />
+        <Ul>
+          {INFORMATION.map(({ title, link }) => (
+            <Li key={title}>
+              <a href={link}>{title}</a>
+            </Li>
+          ))}
+        </Ul>
       </main>
     </>
   );
 };
 
 export default Information;
+
+const Ul = styled.ul`
+  list-style: none;
+  margin-top: 16px;
+`;
+
+const Li = styled.li(
+  {
+    paddingBlock: '16px',
+  },
+  ({ theme }) => ({
+    borderBottom: `1px solid ${theme.colors.gray2}`,
+    color: theme.colors.gray5,
+    ...theme.typographies.body1,
+  }),
+);
+
+// TODO: 정보별 링크 추가
+const INFORMATION = [
+  { title: '이용약관', link: '' },
+  { title: '개인정보 정책', link: '' },
+  { title: '오픈소스', link: '' },
+];
