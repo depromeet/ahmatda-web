cebf5f5a57d80bb627762f0a5cdb5733ec02a973
Hyesung Oh <haesungoh414@gmail.com>
2022-12-18 01:21:28 +0900

    feat: use-get-user-template 개발

---
+++ src/hooks/api/template/useGetUserTemplate.ts
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilValue } from 'recoil';
+
+import { Category } from '../category/type';
+
+import { UserTemplate } from './type';
+
+import { get } from '@/lib/api';
+import currentCategoryState from '@/store/route-home/currentCategory';
+
+interface Response {
+  result: UserTemplate[];
+}
+
+const getUserTemplate = (categoryId: number) => get<Response>(`/template/user?category=${categoryId}`);
+
+const USER_TEMPLATE_QUERY_KEY = 'user_template';
+
+const useGetUserTemplate = () => {
+  const currentCategory = useRecoilValue(currentCategoryState);
+
+  const query = useQuery({
+    queryKey: [USER_TEMPLATE_QUERY_KEY, currentCategory?.id],
+    queryFn: () => getUserTemplate((currentCategory as Category).id),
+    enabled: Boolean(currentCategory),
+  });
+
+  return { ...query, data: query.data?.result };
+};
+
+export default useGetUserTemplate;
