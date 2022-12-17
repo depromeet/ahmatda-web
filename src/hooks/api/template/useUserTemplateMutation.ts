efeba1671f466e57eb002ef041e9e7be9b7dd05e
Hyesung Oh <haesungoh414@gmail.com>
2022-12-18 04:12:28 +0900

    feat: 사용자 템플릿 수정, 삭제 mutation 추가

--- src/hooks/api/template/useUserTemplateMutation.ts
+++ src/hooks/api/template/useUserTemplateMutation.ts
@@ -3,7 +3,7 @@ import { useRecoilValue } from 'recoil';
 
 import { USER_TEMPLATE_QUERY_KEY } from './useGetUserTemplate';
 
-import { post } from '@/lib/api';
+import { del, patch, post } from '@/lib/api';
 import currentCategoryState from '@/store/route-home/currentCategory';
 
 interface CreateUserTemplateProps {
@@ -20,6 +20,16 @@ interface CreateUserTemplateRequest {
   }[];
 }
 
+interface EditUserTemplateProps {
+  templateId: number;
+  templateName: string;
+  pin: boolean;
+}
+
+interface EditUserTemplateRequest extends EditUserTemplateProps {
+  categoryId: number;
+}
+
 const useUserTemplateMutation = () => {
   const queryClient = useQueryClient();
 
@@ -33,7 +43,7 @@ const useUserTemplateMutation = () => {
 
   const createUserTemplateMutation = useMutation(
     (newUserTemplate: CreateUserTemplateProps) => {
-      if (!currentCategory) throw new Error('카테고리');
+      if (!currentCategory) throw new Error('카테고리 선택 후 생성해 주세요.');
 
       const requestData: CreateUserTemplateRequest = {
         templateName: newUserTemplate.templateName,
@@ -50,7 +60,28 @@ const useUserTemplateMutation = () => {
     },
   );
 
-  return { createUserTemplateMutation };
+  const editUserTemplateMutation = useMutation(
+    (editUserTemplate: EditUserTemplateProps) => {
+      if (!currentCategory) throw new Error('카테고리 선택 후 수정해 주세요.');
+
+      const requestData: EditUserTemplateRequest = { ...editUserTemplate, categoryId: currentCategory.id };
+
+      return patch('/template', requestData);
+    },
+    {
+      onSuccess: () => {
+        resetCurrentUserTemplate();
+      },
+    },
+  );
+
+  const deleteUserTemplateMutation = useMutation((id: number) => del(`/template/${id}`), {
+    onSuccess: () => {
+      resetCurrentUserTemplate();
+    },
+  });
+
+  return { createUserTemplateMutation, editUserTemplateMutation, deleteUserTemplateMutation };
 };
 
 export default useUserTemplateMutation;
