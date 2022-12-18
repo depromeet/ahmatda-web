e3b17a744b19a9040194e063bbe771c70a4cc21c
hansol <dundunki@naver.com>
2022-12-18 16:35:05 +0900

    fix: store 추가 및 수정

--- src/store/route-onboard/selectedOnboardCategory.ts
+++ src/store/route-onboard/selectedOnboardCategory.ts
@@ -1,8 +1,11 @@
 import { atom } from 'recoil';
 
-const selectedOnboardCategory = atom<string>({
+import { ONBOARD_CATEGORY } from '@/constants/route-onboard/onboardConstants';
+import { Category } from '@/hooks/api/category/type';
+
+const selectedOnboardCategory = atom<Category>({
   key: 'selectedOnboardCategory',
-  default: 'DAILY',
+  default: ONBOARD_CATEGORY[0],
 });
 
 export default selectedOnboardCategory;
