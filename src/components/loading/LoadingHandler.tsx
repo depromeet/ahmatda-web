48945f608e305dd7acf5072581a53b106c5f4a40
kooku <koomg9599@gmail.com>
2023-01-03 00:50:58 +0900

    fix: framer children component에 key 추가 (#250)

--- src/components/loading/LoadingHandler.tsx
+++ src/components/loading/LoadingHandler.tsx
@@ -22,7 +22,9 @@ const LoadingHandler: FC<PropsWithChildren<Props>> = ({ children, isLoading, fal
   const id = useId();
 
   return (
-    <AnimatePresence mode="wait">{isLoading ? <Fragment key={id}>{fallback}</Fragment> : children}</AnimatePresence>
+    <AnimatePresence mode="wait">
+      <Fragment key={id}>{isLoading ? fallback : children}</Fragment>
+    </AnimatePresence>
   );
 };
 
