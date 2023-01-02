aeaaa60454b56aa3c07ab0cdf0f6eca80d0f3ce8
kooku0 <koomg9599@gmail.com>
2023-01-03 01:08:05 +0900

    refactor: exception으로 error 핸들링

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,9 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 
+import { errorMessage } from '@/constants/message';
+import ApiException from '@/exceptions/ApiException';
+import CustomException from '@/exceptions/CustomException';
+import { ApiErrorScheme } from '@/models/api';
 import { isProd } from '@/utils/utils';
 
 const instance = axios.create({
@@ -18,8 +22,12 @@ const interceptorResponseFulfilled = (response: AxiosResponse) => {
   return Promise.reject(response.data);
 };
 
-const interceptorResponseRejected = (error: AxiosError<{ error: string }>) => {
-  return Promise.reject(new Error(error.response?.data?.error ?? error.name));
+const interceptorResponseRejected = (error: AxiosError<{ error: ApiErrorScheme }>) => {
+  if (error.response?.data?.error?.message && error.response?.data?.error?.code) {
+    return Promise.reject(new ApiException(error.response.data.error));
+  }
+
+  return Promise.reject(new CustomException(errorMessage.UNKNOWN_400, 'NETWORK_ERROR'));
 };
 
 instance.interceptors.response.use(interceptorResponseFulfilled, interceptorResponseRejected);
