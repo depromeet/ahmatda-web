4ff52f8a726f3f558ee1cdcd4d3c144619d1d067
Hyesung Oh <haesungoh414@gmail.com>
2023-01-03 20:36:41 +0900

    refactor: post-app-message 유틸 타입 확장

--- src/hooks/bridge/postAppMessage.ts
+++ src/hooks/bridge/postAppMessage.ts
@@ -3,7 +3,7 @@ import { PostAppMessageData } from './type';
 /**
  * `Web`에서 `App`으로 데이터를 전송합니다.
  */
-const postAppMessage = ({ type, data, ...rest }: PostAppMessageData) => {
+const postAppMessage = <T>({ type, data, ...rest }: PostAppMessageData<T>) => {
   if (!window?.ReactNativeWebView) return;
 
   window.ReactNativeWebView.postMessage(JSON.stringify({ type, data, ...rest }));
