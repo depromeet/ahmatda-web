aa9340682a3efb91b4fa0591322171f7c3c208be
Hyesung Oh <haesungoh414@gmail.com>
2023-01-03 20:04:02 +0900

    feat: app message 관련 타입 제너릭 사용

--- src/hooks/bridge/type.ts
+++ src/hooks/bridge/type.ts
@@ -1,15 +1,15 @@
-export type LISTENLING_WEBVIEW_MESSAGE_KEY = 'FCM_TOKEN';
+export type LISTENLING_WEBVIEW_MESSAGE_KEY = 'FCM_TOKEN' | 'APP_VERSION';
 
 export type POST_WEBVIEW_MESSAGE_KEY = 'baz';
 
-export interface ListeningAppMessageData {
+export interface ListeningAppMessageData<T = string> {
   type: LISTENLING_WEBVIEW_MESSAGE_KEY;
-  data: unknown;
+  data: T;
   [key: string]: unknown;
 }
 
-export interface PostAppMessageData {
+export interface PostAppMessageData<T = unknown> {
   type: POST_WEBVIEW_MESSAGE_KEY;
-  data: unknown;
+  data: T;
   [key: string]: unknown;
 }
