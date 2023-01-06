615451d0995fad7168868116727ef5c865c87a04
Hyesung Oh <haesungoh414@gmail.com>
2023-01-06 19:27:08 +0900

    fix: record 조건문 변경

--- src/lib/analytics/record.ts
+++ src/lib/analytics/record.ts
@@ -4,7 +4,7 @@ import { mixpanelTrack } from './mixpanel';
 import { isProd } from '@/utils/utils';
 
 const recordEvent = ({ action, category, label, value }: Parameters<typeof gaEvent>[0]) => {
-  if (isProd(process.env.NODE_ENV)) return;
+  if (!isProd(process.env.NODE_ENV)) return;
 
   gaEvent({ action, category, label, value });
   mixpanelTrack(action, { category, label, value });
