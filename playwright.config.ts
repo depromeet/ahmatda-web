dc5dbb1c3cfb7225954a5d23ead54577b4a57207
Hyesung Oh <haesungoh414@gmail.com>
2023-01-23 16:58:46 +0900

    test: web server setting

--- playwright.config.ts
+++ playwright.config.ts
@@ -85,10 +85,10 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'yarn dev',
+    port: 3000,
+  },
 };
 
 export default config;
